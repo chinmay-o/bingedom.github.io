5b1179674bbf469233f7bec49615e2cc9ba746de
chinmay866 <37858624+chinmay866@users.noreply.github.com>
2021-04-23 18:09:31 +0530

    Beta 1.0V
    Responsive Glitches

--- file/index.js
+++ file/index.js
@@ -2,4 +2,7 @@
 setTimeout(function(){
 
   $(".MainContent").css("display", "block");
+  $(".logo-reveal-anim").css("display", "none");
+  $(".logo-reveal-img").css("display", "block");
+
 }, 4800);
