da2a21861f6b7051eddc761b9e70cb7aded21f3e
chinmay866 <37858624+chinmay866@users.noreply.github.com>
2021-04-24 11:33:12 +0530

    Minor Changes
    Favicon
    Animation Timing Changes

--- file/index.js
+++ file/index.js
@@ -1,8 +1,14 @@
 
+
+setTimeout(function(){
+
+  $(".logo-reveal-anim").css("display", "none");
+  $(".logo-reveal-img").css("display", "block");
+
+}, 4220);
+
 setTimeout(function(){
 
   $(".MainContent").css("display", "block");
-  $(".logo-reveal-anim").css("display", "none");
-  $(".logo-reveal-img").css("display", "block");
 
 }, 4800);
