644ebdf63f57f470702760d80a3a87dc36a45f54
chinmay866 <37858624+chinmay866@users.noreply.github.com>
2021-04-25 17:34:32 +0530

    Animation Delay Addition

--- file/index.js
+++ file/index.js
@@ -3,28 +3,28 @@ setTimeout(function(){
 
   $(".logo-reveal-img-pre").css("display", "none");
 
-}, 4600);
+}, 6600);
 
 setTimeout(function(){
 
   $(".logo-reveal-anim").css("display", "block");
 
-}, 4598);
+}, 6598);
 
 setTimeout(function(){
 
   $(".logo-reveal-anim").css("display", "none");
 
-}, 7590);
+}, 9590);
 
 setTimeout(function(){
 
   $(".logo-reveal-img-post").css("display", "block");
 
-}, 7590);
+}, 9590);
 
 setTimeout(function(){
 
   $(".MainContent").css("display", "block");
 
-}, 8000);
+}, 10000);
